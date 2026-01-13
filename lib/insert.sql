d28f9f28b101eaeebb4249cfef27cbb99f0b5a87
Snyder-F8 <fridahnzomo857@gmail.com>
2026-01-13 15:13:43 +0300

    Changes in the sql files and sql_query file

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+    ('Mr. Chocolate', 10, 'M', 'Brown', 'Calm', 1),
+    ('Rowdy', 7, 'M', 'Black', 'Aggressive', 1),
+    ('Tabitha', 5, 'F', 'White', 'Gentle', 1),
+    ('Sergeant Brown', 12, 'M', 'Brown', 'Strict', 1),
+    ('Melissa', 4, 'F', 'Blonde', 'Playful', 1),
+    ('Grinch', 9, 'M', 'Green', 'Grumpy', 0),
+    ('Wendy', 6, 'F', 'Brown', 'Friendly', 1),
+    (NULL, 3, 'F', 'Grey', 'Shy', 1);
