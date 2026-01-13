3483db4d075722bd6e912a6a867e4e75e12e219b
Snyder-F8 <fridahnzomo857@gmail.com>
2026-01-13 15:25:39 +0300

    Changes in the sql_query

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -6,17 +6,25 @@ FROM bears
 WHERE sex = 'F';
 """
 
-
 select_all_bears_ordered_by_age = """
-SELECT *
+SELECT
+    name,
+    age,
+    sex,
+    color,
+    temperament,
+    alive
 FROM bears
 ORDER BY age ASC;
 """
 
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-SELECT *
+SELECT
+    name,
+    age
 FROM bears
-WHERE alive = 1;
+WHERE alive = 1
+ORDER BY age ASC;
 """
 
 select_oldest_bear_and_returns_name_and_age = """
